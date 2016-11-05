c592d5f1eb4f1a7c4f2efae7fbee62b9f21dc40d
MoustafaHussien <moustafa.elmasry2013@gmail.com>
2016-11-05 16:00:47 +0200

    Update __init__.py
    removing hard coded auth

--- twitter API/__init__.py
+++ twitter API/__init__.py
@@ -1,11 +1,6 @@
 import tweepy
 import json
-
-ckey = 'rTWMi8ejikhsyyBdaRbpytVEA'
-csecret = "c180XPEpQTC9eu2k9xCRFvZgqUa2du9yKpnwae6f4GBbRbOcbs"
-atoken = "794364093020913665-xe0uMfb4ypPAylCzcZrmumq54NTMEgg"
-asecret = "lbcuN7e07kJkZxL7QzGoQVvm0JbO4vTovGDKkXa465QmS"
-
+# please add your auth
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
