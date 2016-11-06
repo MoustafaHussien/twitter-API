7fbdfb0a8238487bbd0cd53c8f7e03fba9615898
MoustafaHussien <moustafa.elmasry2013@gmail.com>
2016-11-06 15:25:47 +0200

    fixing time bug

--- twitter API/__init__.py
+++ twitter API/__init__.py
@@ -1,6 +1,10 @@
-import tweepy, datetime
+import tweepy
+import datetime
 
-
+ckey = 'rTWMi8ejikhsyyBdaRbpytVEA'
+csecret = "c180XPEpQTC9eu2k9xCRFvZgqUa2du9yKpnwae6f4GBbRbOcbs"
+atoken = "794364093020913665-xe0uMfb4ypPAylCzcZrmumq54NTMEgg"
+asecret = "lbcuN7e07kJkZxL7QzGoQVvm0JbO4vTovGDKkXa465QmS"
 
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
@@ -10,7 +14,8 @@ api = tweepy.API(auth)
 def get_all_tweets(screen_name):
     tweets = api.user_timeline(screen_name=screen_name, count=200)
     for tweet in tweets:
-        if (datetime.datetime.utcnow() - tweet.created_at).seconds < (60 * 60):
+        x = (datetime.datetime.utcnow() - tweet.created_at)
+        if (x.days < 1) and (x.seconds < (60 * 60)):
             print tweet.text.encode("utf-8")
 
 
